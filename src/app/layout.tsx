33bf08dcac72d6e75ec4261572b09aa8d75d287d
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-05-06 18:36:00 -0600

    Initial

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const inter = Montserrat({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "CVS0 - CDN",
+  description: "My personal image CDN for my projects.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="mx-auto max-w-4xl px-6">
+      <h2 className="mt-10 text-6xl text-center font-extrabold">
+        <a href='https://cvs0.xyz' className="hover:underline hover:text-blue-500">cvs0</a> - <span className="text-blue-500">CDN</span>
+      </h2>
+
+      <p className="text-neutral-600 text-center font-semibold">
+        This is the CDN (Content Delivery Network) for all of my applications.
+      </p>
+    </div>
+  );
+}
